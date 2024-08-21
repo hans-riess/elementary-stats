3d35fe450766d5f2d08ed0dc449992da65d019d8
hans-riess <hans.riess@duke.edu>
2024-08-21 12:30:01 -0400

    prepare djano app for heroku

--- polls/forms.py
+++ polls/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from .models import Response
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 
 class ResponseForm(forms.ModelForm):
     class Meta:
         model = Response
         fields = ['answer']
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
 
 class ResultsForm(forms.ModelForm):
     pass
