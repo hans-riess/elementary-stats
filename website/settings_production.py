34f8f1687bb0819dfb6d60790b37802cb0257f42
hans-riess <hans.riess@duke.edu>
2024-08-21 13:13:20 -0400

    postgress

--- website/settings_production.py
+++ website/settings_production.py
@@ -12,15 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600, ssl_require=True
+    )
 }
+
 
 
 # Quick-start development settings - unsuitable for production
